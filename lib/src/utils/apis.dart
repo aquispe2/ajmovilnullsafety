b2ca981c2224a85687a596319cc8b86b4affaa73
alvaro quispe <alvaroquispesegales@gmail.com>
2021-06-04 19:59:22 -0400

    funciona camara

---
+++ lib/src/utils/apis.dart
@@ -0,0 +1,21 @@
+//====PRODUDCION
+const API_NET = "https://siaj.aj.gob.bo:40101/api";
+const API_JAVA = "http://ajmovil.aj.gob.bo:50090/api/v1";
+const SITIO_WEB = "https://siaj.aj.gob.bo:50070";
+//const API_AJAYU = "http://192.168.2.121:50060/api";
+const API_AJAYU = "https://siaj.aj.gob.bo:50060/api"; // para produccion afuera
+const PATH_FILE = "adjuntos/open/file?filename=/home/srvadmpw/Adjuntos";
+const SITIO_SIAJ = "https://siaj.aj.gob.bo:40085";
+//const TOPIC = 'anuncios_aj_prd';
+
+//====PRUEBAS
+//const API_NET = "http://192.168.2.205:40090/api";
+//const SITIO_WEB = "http://192.168.3.135:50070";
+//const API_AJAYU = "http://192.168.3.135:50084/api";
+//const API_JAVA = "http://ajmovil.aj.gob.bo:50090/api/v1";
+//const PATH_FILE = "adjuntos/open/file?filename=/home/srvadmpw/Adjuntos/";
+const TOPIC = 'anuncios_aj_qas';
+
+//==== OTRAS CONFIGIURACIONES ===============
+const VERSION_APP = 2.0;
+const TIMEOUT_SECOND = 60;
